6ccc8c87381853086043ac9eaa4ba5c4cad70ce9
zzh202408064212 <2794813078@qq.com>
2025-03-18 22:03:21 +0800

    Site updated: 2025-03-18 22:03:21

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html","posts/60710.html"];function toRandomPost(){
+var posts=["posts/16107.html","posts/60710.html","posts/47331.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
