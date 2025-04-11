327ad475289b452fc196cc9933919418da60f174
zzh202408064212 <2794813078@qq.com>
2025-04-11 20:39:35 +0800

    Site updated: 2025-04-11 20:39:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/16107.html","posts/60710.html","posts/25587.html","posts/30877.html","posts/57716.html"];function toRandomPost(){
+var posts=["posts/16107.html","posts/60710.html","posts/25587.html","posts/30877.html","posts/57716.html","posts/1331.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
